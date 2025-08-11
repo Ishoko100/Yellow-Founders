6a63b9190f57b6c4b46fa8cd73c1a4aec8dc599b
Ishoko100 <apex@mitdrone.com>
2025-08-11 15:48:56 +0100

    Add files via upload

---
+++ _next/static/chunks/app/layout-c472d15f5cfaec5d.js
@@ -0,0 +1,29 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5531:function(e,r,t){"use strict";t.d(r,{Z:function(){return a}});var n=t(2265);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let s=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),l=(...e)=>e.filter((e,r,t)=>!!e&&t.indexOf(e)===r).join(" ");/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var o={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let i=(0,n.forwardRef)(({color:e="currentColor",size:r=24,strokeWidth:t=2,absoluteStrokeWidth:s,className:i="",children:a,iconNode:c,...d},f)=>(0,n.createElement)("svg",{ref:f,...o,width:r,height:r,stroke:e,strokeWidth:s?24*Number(t)/Number(r):t,className:l("lucide",i),...d},[...c.map(([e,r])=>(0,n.createElement)(e,r)),...Array.isArray(a)?a:[a]])),a=(e,r)=>{let t=(0,n.forwardRef)(({className:t,...o},a)=>(0,n.createElement)(i,{ref:a,iconNode:r,className:l(`lucide-${s(e)}`,t),...o}));return t.displayName=`${e}`,t}},5710:function(e,r,t){Promise.resolve().then(t.t.bind(t,8877,23)),Promise.resolve().then(t.bind(t,8136)),Promise.resolve().then(t.t.bind(t,4724,23)),Promise.resolve().then(t.t.bind(t,3247,23))},8136:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return d}});var n=t(7437),s=t(1396),l=t.n(s),o=t(2265),i=t(5531);/**
+ * @license lucide-react v0.446.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let a=(0,i.Z)("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]),c=(0,i.Z)("Menu",[["line",{x1:"4",x2:"20",y1:"12",y2:"12",key:"1e0a9i"}],["line",{x1:"4",x2:"20",y1:"6",y2:"6",key:"1owob3"}],["line",{x1:"4",x2:"20",y1:"18",y2:"18",key:"yk5zj1"}]]);function d(){let[e,r]=(0,o.useState)(!1),t=[{href:"/discover",label:"Discover"},{href:"/create",label:"Create"},{href:"/investors",label:"Investors"},{href:"/about",label:"About"}];return(0,n.jsx)("nav",{className:"bg-white shadow-sm border-b border-gray-100 sticky top-0 z-50",children:(0,n.jsxs)("div",{className:"max-w-7xl mx-auto px-4 sm:px-6 lg:px-8",children:[(0,n.jsxs)("div",{className:"flex justify-between items-center h-16",children:[(0,n.jsxs)(l(),{href:"/",className:"flex items-center space-x-2",children:[(0,n.jsx)("div",{className:"w-8 h-8 bg-yellow-400 rounded-lg flex items-center justify-center",children:(0,n.jsx)("span",{className:"text-gray-900 font-bold text-xl",children:"Y"})}),(0,n.jsx)("span",{className:"text-xl font-bold text-gray-900",children:"Yellow Founders"})]}),(0,n.jsxs)("div",{className:"hidden md:flex items-center space-x-8",children:[t.map(e=>(0,n.jsx)(l(),{href:e.href,className:"text-gray-600 hover:text-yellow-600 transition-colors duration-200 font-medium",children:e.label},e.href)),(0,n.jsx)(l(),{href:"/signin",className:"bg-yellow-400 hover:bg-yellow-500 text-gray-900 px-4 py-2 rounded-lg font-medium transition-colors duration-200",children:"Sign In"})]}),(0,n.jsx)("div",{className:"md:hidden",children:(0,n.jsx)("button",{onClick:()=>r(!e),className:"text-gray-600 hover:text-gray-900 transition-colors duration-200",children:e?(0,n.jsx)(a,{size:24}):(0,n.jsx)(c,{size:24})})})]}),e&&(0,n.jsx)("div",{className:"md:hidden bg-white border-t border-gray-100",children:(0,n.jsxs)("div",{className:"px-2 pt-2 pb-3 space-y-1",children:[t.map(e=>(0,n.jsx)(l(),{href:e.href,className:"block px-3 py-2 text-gray-600 hover:text-yellow-600 transition-colors duration-200",onClick:()=>r(!1),children:e.label},e.href)),(0,n.jsx)(l(),{href:"/signin",className:"block px-3 py-2 bg-yellow-400 hover:bg-yellow-500 text-gray-900 rounded-lg font-medium transition-colors duration-200 mt-2",onClick:()=>r(!1),children:"Sign In"})]})})]})})}},8877:function(){},3247:function(e){e.exports={style:{fontFamily:"'__Inter_e8ce0c', '__Inter_Fallback_e8ce0c'",fontStyle:"normal"},className:"__className_e8ce0c"}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),s=Symbol.for("react.element"),l=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),o=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function a(e,r,t){var n,a={},c=null,d=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(d=r.ref),r)l.call(r,n)&&!i.hasOwnProperty(n)&&(a[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===a[n]&&(a[n]=r[n]);return{$$typeof:s,type:e,key:c,ref:d,props:a,_owner:o.current}}r.jsx=a,r.jsxs=a},7437:function(e,r,t){"use strict";e.exports=t(622)},1396:function(e,r,t){e.exports=t(4724)}},function(e){e.O(0,[724,971,864,744],function(){return e(e.s=5710)}),_N_E=e.O()}]);
